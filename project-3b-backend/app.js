a55b6930a8a3f771e68340681c80dbe6c15863b0
Lakshay Bhala <bhalal@cardiff.ac.uk>
2021-01-30 20:11:37 +0000

    Initial commit

---
+++ project-3b-backend/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(bodyParser.json()); // application/json
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+// Registering the error-handling middleware
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
+mongoose
+  .connect(
+    'mongodb+srv://project-3b-user:UPNuanvNb7in5mse@cluster0.xvxxm.mongodb.net/project-3b?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+      useUnifiedTopology: true
+    }
+  )
+  .then(result => {
+    app.listen(8080);
+  })
+  .catch(err => console.log(err));
---
+++ project-3b-frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = props => {
+  return (
+    <div>
+      <p>Code here.</p>
+    </div>
+  );
+};
+
+export default App;
