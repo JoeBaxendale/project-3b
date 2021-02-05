43c9e0401cf796362c2a15c0c1eb91db71cf3f75
Laura Camilleri <camilleril2@cardiff.ac.uk>
2021-02-05 15:43:11 +0000

    Adjusting css

--- project-3b-frontend/src/containers/ModalAddBar/Modal.js
+++ project-3b-frontend/src/containers/ModalAddBar/Modal.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import Button from "@material-ui/core/Button";
-
 // close button
 //need add form component link here
-
+import './Modal.css'
 const Modal = (props) => {
   const { closeModal } = props;
 
   return (
     <div className="overlay">
       <div className="content">
-        {props.children}
+        <h2>Add Bar</h2>
+        <p>Name</p>
         <textarea placeholder="form to add new bar link to component"/>
-        <Button variant="contained" color="primary" onClick={closeModal}> Cancel</Button>
 
+        <p>Shift Type</p>
+
+        <p>Start Time</p>
+
+        <p>End Time</p>
+
+        <p>Colour</p>
+
+        <Button variant="contained" color="primary" onClick={closeModal}> Submit</Button>
+        <Button variant="contained" color="secondary" onClick={closeModal}> Cancel</Button>
       </div>
 
     </div>
--- project-3b-frontend/src/containers/ModalAddBar/ModalConector.js
+++ project-3b-frontend/src/containers/ModalAddBar/ModalConector.js
@@ -1,16 +1,13 @@
 import React,  { useState } from 'react';
-import './Modal.css';
 import Modal from "./Modal";
 function ModalConnector() {
   const [status, setStatus] = useState(false);
   return (
     <div>
-      { status && (<Modal closeModal={() => setStatus(false)}><h2>Add To Schedule</h2></Modal>)}
-      <div className="container">
+      { status && (<Modal closeModal={() => setStatus(false)}></Modal>)}
         <button type="button" className="gantt-control-button" id="new-task" onClick={() => setStatus(true)}>
           Add New Bar
         </button>
-      </div>
 
     </div>
   );
