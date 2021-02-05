3b9b4043ffc0ef351c1745f0f3eaaabb8d4e2bec
Lakshay Bhala <bhalal@cardiff.ac.uk>
2021-02-05 14:43:32 +0000

    Add navigation bar and other layout code

--- project-3b-frontend/src/App.js
+++ project-3b-frontend/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+import Layout from './hoc/Layout/Layout';
 import SvelteGanttReact from './containers/SvelteGanttReact/SvelteGanttReact';
 
 const App = props => {
   return (
     <div>
-      <p>Code here.</p>
-      <SvelteGanttReact></SvelteGanttReact>
+      <Layout>
+        <Switch>
+          <Route path="/gantt-charts" exact component={SvelteGanttReact} />
+          <Redirect to="/gantt-charts" />
+        </Switch>
+      </Layout>
     </div>
   );
 };
