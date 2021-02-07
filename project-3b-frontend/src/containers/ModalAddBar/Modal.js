6fb925c0edb03366db3eb006990d39c36d856b05
Laura Camilleri <camilleril2@cardiff.ac.uk>
2021-02-07 18:12:16 +0000

    creating form with input boxes

--- project-3b-frontend/src/containers/ModalAddBar/Modal.js
+++ project-3b-frontend/src/containers/ModalAddBar/Modal.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import Button from "@material-ui/core/Button";
+import classes from './Modal.module.css';
 
-import './Modal.css'
 const Modal = (props) => {
   const { closeModal } = props;
 
   return (
-    <div className="overlay">
-      <div className="content">
+    <div className={classes.overlay}>
+      <div className={classes.content}>
+        <form>
         <h1>Add Bar</h1>
         <p>Name</p>
-        <textarea placeholder="form to add new bar link to component"/>
+          <input type="text"/>
+        <p>Label</p>
+          <input type="text"/>
 
         <p>Start Time</p>
 
         <p>End Time</p>
 
-        <p>Colour</p>
+        <p>Bar Colour</p>
+
 
         <Button variant="contained" color="primary" onClick={closeModal}> Submit</Button>
         <Button variant="contained" color="secondary" onClick={closeModal}> Cancel</Button>
+        </form>
       </div>
 
     </div>
