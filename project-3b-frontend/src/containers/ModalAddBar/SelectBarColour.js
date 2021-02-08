8ac327bd12b5004054f79b654b40f1ee03ce4f1f
Laura Camilleri <camilleril2@cardiff.ac.uk>
2021-02-08 20:17:28 +0000

    Colour dropdown selector design changes

--- project-3b-frontend/src/containers/ModalAddBar/SelectBarColour.js
+++ project-3b-frontend/src/containers/ModalAddBar/SelectBarColour.js
@@ -1,15 +1,24 @@
 import React, { useState } from 'react';
 import classes from './ModalConnector.module.css';
 
-const SelectBarColour = () => {
+const SelectBarColour = props => {
+  const [colour, setColour] = useState('');
+
+  const handleSelect = e => {
+    setColour(e);
+    console.log('colour selected');
+  };
+
   return (
     <div>
-      <p>Select a bar colour</p>
-      <select>
-        <option>Select</option>
-        <option className={classes.barColour2}>Orange</option>
-        <option className={classes.barColour1}>Green</option>
-      </select>
+      <p>Select Bar Colour</p>
+      <div className={classes.dropDown}>
+        <select className={classes.dropDown} onSelect={handleSelect}>
+          <option>Select</option>
+          <option className={classes.barColour2}>Orange</option>
+          <option className={classes.barColour1}>Green</option>
+        </select>
+      </div>
     </div>
   );
 };
