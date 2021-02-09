3c82bdeed695e90c82e8ee4a3cb7817930e9ee7e
Laura Camilleri <camilleril2@cardiff.ac.uk>
2021-02-09 21:46:35 +0000

    populating database with tennis court data

--- project-3b-backend/controllers/gantt.js
+++ project-3b-backend/controllers/gantt.js
@@ -29,6 +29,79 @@ exports.getData = async (req, res, next) => {
   // firstRow.save();
   // secondRow.save();
 
+  const firstRow = new Row({ label: 'Tennis Court A', type: 'TENNIS_COURT' });
+  const secondRow = new Row({ label: 'Tennis Court B', type: 'TENNIS_COURT' });
+  const thirdRow = new Row({ label: 'Tennis Court C', type: 'TENNIS_COURT' });
+  const fourthRow = new Row({ label: 'Tennis Court D', type: 'TENNIS_COURT' });
+  const fifthRow = new Row({ label: 'Tennis Court E', type: 'TENNIS_COURT' });
+  const sixthRow = new Row({ label: 'Tennis Court E', type: 'TENNIS_COURT' });
+  await firstRow.save();
+  await secondRow.save();
+  await thirdRow.save();
+  await fourthRow.save();
+  await fifthRow.save();
+  await sixthRow.save();
+  const firstTask = new Task({
+    row: firstRow,
+    label: 'Scheduled Slot',
+    from: moment('07:00', 'HH:mm'),
+    to: moment('09:00', 'HH:mm'),
+    classes: 'orange'
+  });
+  const secondTask = new Task({
+    row: secondRow,
+    label: 'Scheduled Slot',
+    from: moment('08:00', 'HH:mm'),
+    to: moment('10:00', 'HH:mm'),
+    classes: 'green'
+  });
+  const thridTask = new Task({
+    row: thirdRow,
+    label: 'Scheduled Slot',
+    from: moment('10:00', 'HH:mm'),
+    to: moment('11:00', 'HH:mm'),
+    classes: 'green'
+  });
+  const fourthTask = new Task({
+    row: fourthRow,
+    label: 'Scheduled Slot',
+    from: moment('06:00', 'HH:mm'),
+    to: moment('09:00', 'HH:mm'),
+    classes: 'orange'
+  });
+  const fifthTask = new Task({
+    row: fifthRow,
+    label: 'Scheduled Slot',
+    from: moment('08:00', 'HH:mm'),
+    to: moment('10:00', 'HH:mm'),
+    classes: 'orange'
+  });
+  const sixthTask = new Task({
+    row: sixthRow,
+    label: 'Scheduled Slot',
+    from: moment('06:00', 'HH:mm'),
+    to: moment('09:00', 'HH:mm'),
+    classes: 'green'
+  });
+  await firstTask.save();
+  await secondTask.save();
+  await thirdTask.save();
+  await fourthTask.save();
+  await fifthTask.save();
+  await sixthTask.save();
+  firstRow.tasks.push(firstTask);
+  secondRow.tasks.push(secondTask);
+  thirdRow.tasks.push(thridTask);
+  fourthRow.tasks.push(fourthTask);
+  fifthRow.tasks.push(fifthTask);
+  sixthRow.tasks.push(sixthTask);
+  firstRow.save();
+  secondRow.save();
+  thirdRow.save();
+  fourthRow.save();
+  fifthRow.save();
+  sixthRow.save();
+
   const ganttChartType = req.params.ganttChartType;
 
   try {
