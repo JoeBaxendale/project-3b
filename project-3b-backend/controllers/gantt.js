e9b9f482b7632821703d5a9ac16a7b3d0a0db809
Lakshay Bhala <bhalal@cardiff.ac.uk>
2021-02-10 19:25:19 +0000

    Second temporary fix for IDs

--- project-3b-backend/controllers/gantt.js
+++ project-3b-backend/controllers/gantt.js
@@ -4,103 +4,143 @@ const Row = require('../models/row');
 const Task = require('../models/task');
 
 exports.getData = async (req, res, next) => {
-  // const firstRow = new Row({ label: 'Field Engineer A', type: 'FIELD_ENGINEER' });
-  // const secondRow = new Row({ label: 'Field Engineer B', type: 'FIELD_ENGINEER' });
-  // await firstRow.save();
-  // await secondRow.save();
-  // const firstTask = new Task({
-  //   row: firstRow,
+  // const engineerFirstRow = new Row({
+  //   _id: '5c0f66b979af55031b347101',
+  //   label: 'Field Engineer A',
+  //   type: 'FIELD_ENGINEER'
+  // });
+  // const engineerSecondRow = new Row({
+  //   _id: '5c0f66b979af55031b347102',
+  //   label: 'Field Engineer B',
+  //   type: 'FIELD_ENGINEER'
+  // });
+  // await engineerFirstRow.save();
+  // await engineerSecondRow.save();
+  // const engineerFirstTask = new Task({
+  //   _id: '5c0f66b979af55031b347201',
+  //   row: engineerFirstRow,
   //   label: 'Scheduled Shift',
   //   from: moment('07:00', 'HH:mm'),
   //   to: moment('09:00', 'HH:mm'),
   //   classes: 'orange'
   // });
-  // const secondTask = new Task({
-  //   row: secondRow,
+  // const engineerSecondTask = new Task({
+  //   _id: '5c0f66b979af55031b347202',
+  //   row: engineerSecondRow,
   //   label: 'Scheduled Shift',
   //   from: moment('07:00', 'HH:mm'),
   //   to: moment('09:00', 'HH:mm'),
   //   classes: 'green'
   // });
-  // await firstTask.save();
-  // await secondTask.save();
-  // firstRow.tasks.push(firstTask);
-  // secondRow.tasks.push(secondTask);
-  // firstRow.save();
-  // secondRow.save();
+  // await engineerFirstTask.save();
+  // await engineerSecondTask.save();
+  // engineerFirstRow.tasks.push(engineerFirstTask);
+  // engineerSecondRow.tasks.push(engineerSecondTask);
+  // await engineerFirstRow.save();
+  // await engineerSecondRow.save();
 
-  const firstRow = new Row({ label: 'Tennis Court A', type: 'TENNIS_COURT' });
-  const secondRow = new Row({ label: 'Tennis Court B', type: 'TENNIS_COURT' });
-  const thirdRow = new Row({ label: 'Tennis Court C', type: 'TENNIS_COURT' });
-  const fourthRow = new Row({ label: 'Tennis Court D', type: 'TENNIS_COURT' });
-  const fifthRow = new Row({ label: 'Tennis Court E', type: 'TENNIS_COURT' });
-  const sixthRow = new Row({ label: 'Tennis Court E', type: 'TENNIS_COURT' });
-  await firstRow.save();
-  await secondRow.save();
-  await thirdRow.save();
-  await fourthRow.save();
-  await fifthRow.save();
-  await sixthRow.save();
-  const firstTask = new Task({
-    row: firstRow,
-    label: 'Scheduled Slot',
-    from: moment('07:00', 'HH:mm'),
-    to: moment('09:00', 'HH:mm'),
-    classes: 'orange'
-  });
-  const secondTask = new Task({
-    row: secondRow,
-    label: 'Scheduled Slot',
-    from: moment('08:00', 'HH:mm'),
-    to: moment('10:00', 'HH:mm'),
-    classes: 'green'
-  });
-  const thridTask = new Task({
-    row: thirdRow,
-    label: 'Scheduled Slot',
-    from: moment('10:00', 'HH:mm'),
-    to: moment('11:00', 'HH:mm'),
-    classes: 'green'
-  });
-  const fourthTask = new Task({
-    row: fourthRow,
-    label: 'Scheduled Slot',
-    from: moment('06:00', 'HH:mm'),
-    to: moment('09:00', 'HH:mm'),
-    classes: 'orange'
-  });
-  const fifthTask = new Task({
-    row: fifthRow,
-    label: 'Scheduled Slot',
-    from: moment('08:00', 'HH:mm'),
-    to: moment('10:00', 'HH:mm'),
-    classes: 'orange'
-  });
-  const sixthTask = new Task({
-    row: sixthRow,
-    label: 'Scheduled Slot',
-    from: moment('06:00', 'HH:mm'),
-    to: moment('09:00', 'HH:mm'),
-    classes: 'green'
-  });
-  await firstTask.save();
-  await secondTask.save();
-  await thirdTask.save();
-  await fourthTask.save();
-  await fifthTask.save();
-  await sixthTask.save();
-  firstRow.tasks.push(firstTask);
-  secondRow.tasks.push(secondTask);
-  thirdRow.tasks.push(thridTask);
-  fourthRow.tasks.push(fourthTask);
-  fifthRow.tasks.push(fifthTask);
-  sixthRow.tasks.push(sixthTask);
-  firstRow.save();
-  secondRow.save();
-  thirdRow.save();
-  fourthRow.save();
-  fifthRow.save();
-  sixthRow.save();
+  // const tennisFirstRow = new Row({
+  //   _id: '5c0f66b979af55031b347103',
+  //   label: 'Tennis Court A',
+  //   type: 'TENNIS_COURT'
+  // });
+  // const tennisSecondRow = new Row({
+  //   _id: '5c0f66b979af55031b347104',
+  //   label: 'Tennis Court B',
+  //   type: 'TENNIS_COURT'
+  // });
+  // const tennisThirdRow = new Row({
+  //   _id: '5c0f66b979af55031b347105',
+  //   label: 'Tennis Court C',
+  //   type: 'TENNIS_COURT'
+  // });
+  // const tennisFourthRow = new Row({
+  //   _id: '5c0f66b979af55031b347106',
+  //   label: 'Tennis Court D',
+  //   type: 'TENNIS_COURT'
+  // });
+  // const tennisFifthRow = new Row({
+  //   _id: '5c0f66b979af55031b347107',
+  //   label: 'Tennis Court E',
+  //   type: 'TENNIS_COURT'
+  // });
+  // const tennisSixthRow = new Row({
+  //   _id: '5c0f66b979af55031b347108',
+  //   label: 'Tennis Court F',
+  //   type: 'TENNIS_COURT'
+  // });
+  // await tennisFirstRow.save();
+  // await tennisSecondRow.save();
+  // await tennisThirdRow.save();
+  // await tennisFourthRow.save();
+  // await tennisFifthRow.save();
+  // await tennisSixthRow.save();
+  // const tennisFirstTask = new Task({
+  //   _id: '5c0f66b979af55031b347203',
+  //   row: tennisFirstRow,
+  //   label: 'Scheduled Slot',
+  //   from: moment('07:00', 'HH:mm'),
+  //   to: moment('09:00', 'HH:mm'),
+  //   classes: 'blue'
+  // });
+  // const tennisSecondTask = new Task({
+  //   _id: '5c0f66b979af55031b347204',
+  //   row: tennisSecondRow,
+  //   label: 'Scheduled Slot',
+  //   from: moment('08:00', 'HH:mm'),
+  //   to: moment('10:00', 'HH:mm'),
+  //   classes: 'orange'
+  // });
+  // const tennisThirdTask = new Task({
+  //   _id: '5c0f66b979af55031b347205',
+  //   row: tennisThirdRow,
+  //   label: 'Scheduled Slot',
+  //   from: moment('10:00', 'HH:mm'),
+  //   to: moment('11:00', 'HH:mm'),
+  //   classes: 'green'
+  // });
+  // const tennisFourthTask = new Task({
+  //   _id: '5c0f66b979af55031b347206',
+  //   row: tennisFourthRow,
+  //   label: 'Scheduled Slot',
+  //   from: moment('06:00', 'HH:mm'),
+  //   to: moment('09:00', 'HH:mm'),
+  //   classes: 'blue'
+  // });
+  // const tennisFifthTask = new Task({
+  //   _id: '5c0f66b979af55031b347207',
+  //   row: tennisFifthRow,
+  //   label: 'Scheduled Slot',
+  //   from: moment('08:00', 'HH:mm'),
+  //   to: moment('10:00', 'HH:mm'),
+  //   classes: 'orange'
+  // });
+  // const tennisSixthTask = new Task({
+  //   _id: '5c0f66b979af55031b347208',
+  //   row: tennisSixthRow,
+  //   label: 'Scheduled Slot',
+  //   from: moment('06:00', 'HH:mm'),
+  //   to: moment('09:00', 'HH:mm'),
+  //   classes: 'green'
+  // });
+  // await tennisFirstTask.save();
+  // await tennisSecondTask.save();
+  // await tennisThirdTask.save();
+  // await tennisFourthTask.save();
+  // await tennisFifthTask.save();
+  // await tennisSixthTask.save();
+  // tennisFirstRow.tasks.push(tennisFirstTask);
+  // tennisSecondRow.tasks.push(tennisSecondTask);
+  // tennisThirdRow.tasks.push(tennisThirdTask);
+  // tennisFourthRow.tasks.push(tennisFourthTask);
+  // tennisFifthRow.tasks.push(tennisFifthTask);
+  // tennisSixthRow.tasks.push(tennisSixthTask);
+  // await tennisFirstRow.save();
+  // await tennisSecondRow.save();
+  // await tennisThirdRow.save();
+  // await tennisFourthRow.save();
+  // await tennisFifthRow.save();
+  // await tennisSixthRow.save();
 
   const ganttChartType = req.params.ganttChartType;
 
