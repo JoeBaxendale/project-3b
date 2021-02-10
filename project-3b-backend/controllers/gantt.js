5c65273902babc5658f0158d27feb43d445b47ee
Lakshay Bhala <bhalal@cardiff.ac.uk>
2021-02-10 19:31:26 +0000

    Simplify second temporary fix code for IDs

--- project-3b-backend/controllers/gantt.js
+++ project-3b-backend/controllers/gantt.js
@@ -4,143 +4,59 @@ const Row = require('../models/row');
 const Task = require('../models/task');
 
 exports.getData = async (req, res, next) => {
-  // const engineerFirstRow = new Row({
-  //   _id: '5c0f66b979af55031b347101',
-  //   label: 'Field Engineer A',
-  //   type: 'FIELD_ENGINEER'
-  // });
-  // const engineerSecondRow = new Row({
-  //   _id: '5c0f66b979af55031b347102',
-  //   label: 'Field Engineer B',
-  //   type: 'FIELD_ENGINEER'
-  // });
-  // await engineerFirstRow.save();
-  // await engineerSecondRow.save();
-  // const engineerFirstTask = new Task({
-  //   _id: '5c0f66b979af55031b347201',
-  //   row: engineerFirstRow,
-  //   label: 'Scheduled Shift',
-  //   from: moment('07:00', 'HH:mm'),
-  //   to: moment('09:00', 'HH:mm'),
-  //   classes: 'orange'
-  // });
-  // const engineerSecondTask = new Task({
-  //   _id: '5c0f66b979af55031b347202',
-  //   row: engineerSecondRow,
-  //   label: 'Scheduled Shift',
-  //   from: moment('07:00', 'HH:mm'),
-  //   to: moment('09:00', 'HH:mm'),
-  //   classes: 'green'
-  // });
-  // await engineerFirstTask.save();
-  // await engineerSecondTask.save();
-  // engineerFirstRow.tasks.push(engineerFirstTask);
-  // engineerSecondRow.tasks.push(engineerSecondTask);
-  // await engineerFirstRow.save();
-  // await engineerSecondRow.save();
+  // const colours = ['blue', 'green', 'orange'];
 
-  // const tennisFirstRow = new Row({
-  //   _id: '5c0f66b979af55031b347103',
-  //   label: 'Tennis Court A',
-  //   type: 'TENNIS_COURT'
-  // });
-  // const tennisSecondRow = new Row({
-  //   _id: '5c0f66b979af55031b347104',
-  //   label: 'Tennis Court B',
-  //   type: 'TENNIS_COURT'
-  // });
-  // const tennisThirdRow = new Row({
-  //   _id: '5c0f66b979af55031b347105',
-  //   label: 'Tennis Court C',
-  //   type: 'TENNIS_COURT'
-  // });
-  // const tennisFourthRow = new Row({
-  //   _id: '5c0f66b979af55031b347106',
-  //   label: 'Tennis Court D',
-  //   type: 'TENNIS_COURT'
-  // });
-  // const tennisFifthRow = new Row({
-  //   _id: '5c0f66b979af55031b347107',
-  //   label: 'Tennis Court E',
-  //   type: 'TENNIS_COURT'
-  // });
-  // const tennisSixthRow = new Row({
-  //   _id: '5c0f66b979af55031b347108',
-  //   label: 'Tennis Court F',
-  //   type: 'TENNIS_COURT'
-  // });
-  // await tennisFirstRow.save();
-  // await tennisSecondRow.save();
-  // await tennisThirdRow.save();
-  // await tennisFourthRow.save();
-  // await tennisFifthRow.save();
-  // await tennisSixthRow.save();
-  // const tennisFirstTask = new Task({
-  //   _id: '5c0f66b979af55031b347203',
-  //   row: tennisFirstRow,
-  //   label: 'Scheduled Slot',
-  //   from: moment('07:00', 'HH:mm'),
-  //   to: moment('09:00', 'HH:mm'),
-  //   classes: 'blue'
-  // });
-  // const tennisSecondTask = new Task({
-  //   _id: '5c0f66b979af55031b347204',
-  //   row: tennisSecondRow,
-  //   label: 'Scheduled Slot',
-  //   from: moment('08:00', 'HH:mm'),
-  //   to: moment('10:00', 'HH:mm'),
-  //   classes: 'orange'
-  // });
-  // const tennisThirdTask = new Task({
-  //   _id: '5c0f66b979af55031b347205',
-  //   row: tennisThirdRow,
-  //   label: 'Scheduled Slot',
-  //   from: moment('10:00', 'HH:mm'),
-  //   to: moment('11:00', 'HH:mm'),
-  //   classes: 'green'
-  // });
-  // const tennisFourthTask = new Task({
-  //   _id: '5c0f66b979af55031b347206',
-  //   row: tennisFourthRow,
-  //   label: 'Scheduled Slot',
-  //   from: moment('06:00', 'HH:mm'),
-  //   to: moment('09:00', 'HH:mm'),
-  //   classes: 'blue'
-  // });
-  // const tennisFifthTask = new Task({
-  //   _id: '5c0f66b979af55031b347207',
-  //   row: tennisFifthRow,
-  //   label: 'Scheduled Slot',
-  //   from: moment('08:00', 'HH:mm'),
-  //   to: moment('10:00', 'HH:mm'),
-  //   classes: 'orange'
-  // });
-  // const tennisSixthTask = new Task({
-  //   _id: '5c0f66b979af55031b347208',
-  //   row: tennisSixthRow,
-  //   label: 'Scheduled Slot',
-  //   from: moment('06:00', 'HH:mm'),
-  //   to: moment('09:00', 'HH:mm'),
-  //   classes: 'green'
-  // });
-  // await tennisFirstTask.save();
-  // await tennisSecondTask.save();
-  // await tennisThirdTask.save();
-  // await tennisFourthTask.save();
-  // await tennisFifthTask.save();
-  // await tennisSixthTask.save();
-  // tennisFirstRow.tasks.push(tennisFirstTask);
-  // tennisSecondRow.tasks.push(tennisSecondTask);
-  // tennisThirdRow.tasks.push(tennisThirdTask);
-  // tennisFourthRow.tasks.push(tennisFourthTask);
-  // tennisFifthRow.tasks.push(tennisFifthTask);
-  // tennisSixthRow.tasks.push(tennisSixthTask);
-  // await tennisFirstRow.save();
-  // await tennisSecondRow.save();
-  // await tennisThirdRow.save();
-  // await tennisFourthRow.save();
-  // await tennisFifthRow.save();
-  // await tennisSixthRow.save();
+  // for (let i = 0; i < 4; i++) {
+  //   const randomHour = (Math.random() * 10) | 0;
+  //   const randomDay = (Math.random() * 5) | (0 + 1);
+
+  //   let row = new Row({
+  //     _id: `5c0f66b979af55031b34710${i}`,
+  //     label: 'Field Engineer ' + String.fromCharCode(65 + i),
+  //     type: 'FIELD_ENGINEER'
+  //   });
+
+  //   await row.save();
+
+  //   const task = new Task({
+  //     _id: `5c0f66b979af55031b34720${i}`,
+  //     row: row,
+  //     label: 'Scheduled Shift',
+  //     from: moment(`${randomHour}:00`, 'HH:mm'),
+  //     to: moment(`${randomHour + randomDay}:00`, 'HH:mm'),
+  //     classes: colours[(Math.random() * colours.length) | 0]
+  //   });
+
+  //   await task.save();
+  //   row.tasks.push(task);
+  //   await row.save();
+  // }
+
+  // for (let i = 4; i < 10; i++) {
+  //   const randomHour = (Math.random() * 10) | 0;
+  //   const randomDay = (Math.random() * 5) | (0 + 1);
+
+  //   let row = new Row({
+  //     _id: `5c0f66b979af55031b34710${i}`,
+  //     label: 'Tennis Court ' + String.fromCharCode(65 + i),
+  //     type: 'TENNIS_COURT'
+  //   });
+
+  //   await row.save();
+
+  //   const task = new Task({
+  //     _id: `5c0f66b979af55031b34720${i}`,
+  //     row: row,
+  //     label: 'Scheduled Slot',
+  //     from: moment(`${randomHour}:00`, 'HH:mm'),
+  //     to: moment(`${randomHour + randomDay}:00`, 'HH:mm'),
+  //     classes: colours[(Math.random() * colours.length) | 0]
+  //   });
+
+  //   await task.save();
+  //   row.tasks.push(task);
+  //   await row.save();
+  // }
 
   const ganttChartType = req.params.ganttChartType;
 
