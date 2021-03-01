961c5a48c294ccb916c358e61fb1aadc3a9d9ef6
Lakshay Bhala <bhalal@cardiff.ac.uk>
2021-03-01 16:36:49 +0000

    Merge Request: `14-create-new-chart` into `develop`

--- project-3b-frontend/src/App.js
+++ project-3b-frontend/src/App.js
@@ -4,14 +4,16 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import SvelteGanttReact from './containers/SvelteGanttReact/SvelteGanttReact';
 import GanttChartTypes from './containers/GanttChartTypes/GanttChartTypes';
+import DemoNewChart from './containers/DemoNewChart/DemoNewChart';
 
 const App = props => {
   return (
     <div>
       <Layout>
         <Switch>
+          <Route path="/gantt-charts/demo-new-chart" component={DemoNewChart} />
+          <Route path="/gantt-charts/:chartType" component={SvelteGanttReact} />
           <Route path="/gantt-charts" exact component={GanttChartTypes} />
-          <Route path="/gantt-charts/:chartType" exact component={SvelteGanttReact} />
           <Redirect to="/gantt-charts" />
         </Switch>
       </Layout>
